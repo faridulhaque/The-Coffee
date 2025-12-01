1310f7802183cf90a98208ccd21f52aa2665d5c2
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-12-01 16:15:48 +0600

    navbar added with basic setup

---
+++ components/HomeContainer.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+
+function HomeContainer() {
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(() => {
+    const checkScrollTop = () => {
+      if (!showScroll && window.pageYOffset > 0.75 * window.innerHeight) {
+        setShowScroll(true);
+      } else if (
+        showScroll &&
+        window.pageYOffset <= 0.75 * window.innerHeight
+      ) {
+        setShowScroll(false);
+      }
+    };
+    window.addEventListener("scroll", checkScrollTop);
+    return () => {
+      window.removeEventListener("scroll", checkScrollTop);
+    };
+  }, [showScroll]);
+
+  return (
+    <main>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <section></section>
+      {/* footer tag's and the image inside it have styles in the global css file  */}
+      <footer className="footer"></footer>
+
+      <span
+        onClick={() => {
+          window.scrollTo(0, 0);
+        }}
+        className={`puller ${!showScroll ? "onTop" : "onScroll"}`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="white"
+          className="chevron-icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
+          />
+        </svg>
+      </span>
+    </main>
+  );
+}
+
+export default HomeContainer;
---
+++ others/constant.ts
@@ -0,0 +1,46 @@
+export const navItemsEn = [
+  {
+    title: "Home",
+    link: "#home",
+  },
+  {
+    title: "Menu",
+    link: "#menu",
+  },
+  {
+    title: "About",
+    link: "#about",
+  },
+  {
+    title: "Products",
+    link: "#products",
+  },
+  {
+    title: "Contact",
+    link: "#contact",
+  },
+];
+
+export const navItemsAr = [
+  {
+    title: "اتصل بنا",
+    link: "#contact",
+  },
+
+  {
+    title: "المنتجات",
+    link: "#products",
+  },
+  {
+    title: "من نحن",
+    link: "#about",
+  },
+  {
+    title: "القائمة",
+    link: "#menu",
+  },
+  {
+    title: "الرئيسية",
+    link: "#home",
+  },
+];
